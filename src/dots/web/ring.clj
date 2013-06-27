68c6c429d1c8d5cff102d38a33a178051c5fe82f
andts <andts1000@gmail.com>
2013-06-26 23:01:41 -0400

    added routing macro for websockets added some info on necessary server-side actions

--- src/dots/web/ring.clj
+++ src/dots/web/ring.clj
@@ -5,21 +5,26 @@
             [ring.middleware.params :as p]
             [clojure.data.json :as json]
             [dots.core.player :as player]
-            [dots.core.game :as game])
-  )
+            [dots.core.game :as game]))
+
+(defmacro process-request
+  [fn-name & actions]
+  `(defn ~fn-name
+     [~'request]
+     (clojure.core.match/match [(:action ~'request)]
+       ~@actions)))
+
+(process-request route
+  ["get-players"] (player/load-all-players)
+  ["get-player"] (player/load-player (:id request)))
 
 (defn websocket-handler [request]
   (hk/with-channel request channel
     (hk/on-close channel (fn [status] (println "channel closed: " status)))
     (hk/on-receive channel (fn [data]
-                             (do
-                               (println data)
-                               (let [request-data (json/read-str data :key-fn keyword)
-                                     response (m/match [(:request request-data)]
-                                                ["get-players"] (player/load-all-players)
-                                                ["get-player"] (player/load-player (:id request-data))
-                                                )]
-                                 (hk/send! channel (json/write-str response))))))))
+                             (let [request-data (json/read-str data :key-fn keyword)
+                                   response (route request-data)]
+                               (hk/send! channel (json/write-str response)))))))
 
 (c/defroutes dots-routes
   ;websocket entry point
